bb358f0e4e6eea250ea59a8396cb33be925786e3
VenkateshSajja <20058351@mydbs.ie>
2025-03-07 11:23:52 +0000

    Create DisplayData.js

---
+++ server/Routes/DisplayData.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post('/artData', (req, res) => {
+    try {
+        
+        res.send([global.articles,global.paintingCategory])
+    } catch(error) {
+        console.error(error.message);
+        res.send("Server Error");
+    }
+})
+
+module.exports = router;
